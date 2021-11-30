64275b79123ca38c6b99441bcce39f3354f2161f
Rodrigo Rivera <rodrigorivera@pop-os.localdomain>
2021-11-30 17:21:08 -0500

    react project creation & initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import {wordCount} from "./utils/wordCount";
+import { orderWordCount } from "./utils/orderWordCount";
 
-function App() {
+const App = () => {
+  const [textarea, setTextarea] = useState("Input text here");
+
+  const updateTextarea = (event) => {
+    setTextarea(event.target.value);
+  };
+
+  const countButtonHandler = (e) => {
+    e.preventDefault();
+    let arr = textarea.toString();
+    arr = arr.split(" ")
+    const wordcount = wordCount(arr);
+    const finalArray = orderWordCount(wordcount);
+    console.log(finalArray);
+    
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">Word Frequency App!</h1>
+      <h3 className="description">
+        Let´s see how many words are in you´re text!{" "}
+      </h3>
+      <form className="input-form">
+        <p className="input-label">Input text here:</p>
+        <div className="input-div">
+          {/* <input className="input-text" type="text" placeholder="Input you´re text here" required /> */}
+          <textarea
+            className="input-text"
+            name="input-words"
+            id="input-text"
+            cols="30"
+            rows="10"
+            onChange={updateTextarea}
+            value = {textarea}
+            //onClick={updateTextInput}
+          >
+          </textarea>
+          <button className="input-button" onClick={countButtonHandler}>
+            Count words
+          </button>
+        </div>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
